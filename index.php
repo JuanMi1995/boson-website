b345beca45f283000212e7081d4dc7e3391ad05e
JuanMi1995 <120854344+JuanMi1995@users.noreply.github.com>
2025-10-16 14:32:05 -0600

    optimized index.php

--- index.php
+++ index.php
@@ -1,312 +1,151 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="es">
 <head>
-<meta charset="UTF-8">
-<meta name="description" content="Grupo Boson, Ingeniería 4.0, Automatizaciones, Tableros, Soluciones, Calidad de Energía y proyectos de ingenieria en todo el bajio"/>
-<meta name="keywords" content="Ingeniería 4.0 Automatizacion Tableros Proyectos de ingenieria Calidad de Energía"/>
-<meta name="keyphrases" content="Ingeniería 4.0 Automatizacion Tableros Proyectos de ingenieria Calidad de Energía"/>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"  href="css/estilos.css">
-<link rel="stylesheet"  href="css/bootstrap.css">
-<title>Grupo Boson Soluciones y Actualizaciones</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Grupo Boson, Ingeniería 4.0, Automatización, Tableros, Soluciones, Calidad de Energía y proyectos de ingeniería en todo el Bajío.">
+  <meta name="keywords" content="Ingeniería 4.0, Automatización, Tableros, Proyectos de ingeniería, Calidad de Energía">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Grupo Boson | Soluciones y Actualizaciones</title>
 
+  <!-- Favicon -->
+  <link rel="icon" href="imagenes/favicon.ico" type="image/x-icon">
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TDRN492K');</script>
-<!-- End Google Tag Manager -->
+  <!-- Bootstrap (última versión estable 4.x compatible) -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.2/css/bootstrap.min.css">
 
+  <!-- Estilos propios -->
+  <link rel="stylesheet" href="css/estilos.css">
 
-<!--- Zoom --->
-<script type="text/javascript" src="highslide/highslide-with-html.js"></script>
-<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
-<script type="text/javascript">
-    // Apply the Highslide settings
-    hs.graphicsDir = 'highslide/graphics/';
-    hs.outlineType = 'rounded-white';
-    hs.outlineWhileAnimating = true;
-</script>
-<script type="text/javascript" src="js/swfobject.js"></script>
-<script type="text/javascript">
-
-      var flashvars = {};
-      flashvars.cssSource = "piecemaker.css";
-      flashvars.xmlSource = "photo_list.xml";
-		
-      var params = {};
-      params.play = "true";
-      params.menu = "false";
-      params.scale = "showall";
-      params.wmode = "transparent";
-      params.allowfullscreen = "true";
-      params.allowscriptaccess = "always";
-      params.allownetworking = "all";
-	  
-      swfobject.embedSWF('piecemaker.swf', 'piecemaker', '940', '420', '10', null, flashvars,    
-      params, null);
-    
-</script>
-<script language="javascript" type="text/javascript">
-function clearText(field)
-{
-    if (field.defaultValue == field.value) field.value = '';
-    else if (field.value == '') field.value = field.defaultValue;
-}
-function MM_swapImgRestore() { //v3.0
-  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-}
-function MM_preloadImages() { //v3.0
-  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-}
-
-function MM_findObj(n, d) { //v4.01
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-  if(!x && d.getElementById) x=d.getElementById(n); return x;
-}
-
-function MM_swapImage() { //v3.0
-  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-}
-</script>
-<link rel="stylesheet" type="text/css" href="highslide/highslide.css" />
-
-<!--
-    2) Optionally override the settings defined at the top
-    of the highslide.js file. The parameter hs.graphicsDir is important!
--->
-<!--- Zoom --->
-
-<!--- FACE ANTES </head> --->
-
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.8&appId=1444267039216011";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!--- FACE --->
-
-
-
+  <!-- Google Tag Manager -->
+  <script>
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-TDRN492K');
+  </script>
+  <!-- End Google Tag Manager -->
 </head>
-<body topmargin="0" onload="MM_preloadImages('imagenes/top-1.png')">
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDRN492K"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+<body onload="MM_preloadImages('imagenes/top-1.png')">
 
-<!--- TOP --->
-<?php include ("top.php"); ?>
-<!--- TOP --->
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDRN492K"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
+  <!-- TOP -->
+  <?php include("top.php"); ?>
+  <!-- /TOP -->
 
-<!-------------------------- BANNER ---------------------------->
-<div class="seccion-baner" >
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-  </ol>
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-  
-    <div class="item active">
-    <img src="baner/01.png" width="100%">
-    <div class="carousel-caption d-none d-md-block">
-    <p>&nbsp;</p>
-    <a href="contacto.php"> <button class="btn btn-success"> Mas Informacion </button></a>
-    <p>&nbsp;</p>
+  <!-- BANNER -->
+  <div class="seccion-baner">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+      </ol>
+
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="baner/01.png" class="d-block w-100" alt="Banner 1">
+          <div class="carousel-caption">
+            <a href="contacto.php" class="btn btn-success mt-3">Más Información</a>
+          </div>
+        </div>
+
+        <div class="carousel-item">
+          <img src="baner/02.png" class="d-block w-100" alt="Banner 2">
+          <div class="carousel-caption">
+            <a href="contacto.php" class="btn btn-success mt-3">Más Información</a>
+          </div>
+        </div>
+      </div>
+
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+      </a>
     </div>
-    </div>
-   
-    <div class="item">
-    <img src="baner/02.png" width="100%">
-    <div class="carousel-caption d-none d-md-block">
-    <p>&nbsp;</p>
-    <a href="contacto.php"> <button class="btn btn-success"> Mas Informacion </button></a>
-    <p>&nbsp;</p>
-    </div>
-    </div>
-   
-    
   </div>
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-  <span class="glyphicon glyphicon-chevron-left aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</div>
-<!-------------------------- BANNER ---------------------------->
+  <!-- /BANNER -->
 
+  <!-- NOSOTROS -->
+  <section class="seccion-color" id="seccion-color">
+    <img src="imagenes/sombra-top.png" width="100%" alt="Sombra decorativa">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <?php
+          include("conectarse.php");
+          $consulta = mysqli_query($conexion,"SELECT * FROM contenidos WHERE seccion='portada'");
+          while($re = mysqli_fetch_array($consulta)) { ?>
+            <h1><span class="color1"><?php echo $re['titulo'];?></span></h1>
+            <h3><?php echo $re['subtitulo'];?></h3>
+            <div class="text-justify"><?php echo $re['contenido'];?></div>
+            <a href="<?php echo $re['Link'];?>" class="btn btn-success mt-3">Ver Más</a>
+          <?php } ?>
+        </div>
+        <div class="col-md-6 text-center">
+          <img src="imagenes/nosotros.png" class="img-fluid" alt="Nosotros">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /NOSOTROS -->
 
+  <!-- SERVICIOS -->
+  <section class="py-5">
+    <div class="container">
+      <div class="text-center mb-5">
+        <h1 class="color2">Nuestros Servicios</h1>
+      </div>
+      <div class="row">
+        <?php
+        include("conectarse.php");
+        $consulta = mysqli_query($conexion,"SELECT * FROM categorias WHERE Estado='Activo' AND Menu='Principal' ORDER BY Id ASC");
+        while($re = mysqli_fetch_array($consulta)) { ?>
+          <div class="col-sm-6 col-md-4 mb-4 text-center">
+            <a href="<?php echo $re['Link'];?>">
+              <img src="imagenes/<?php echo $re['Foto'];?>" class="img-fluid zoom mb-3" alt="<?php echo $re['Nombre'];?>">
+            </a>
+            <h4><?php echo $re['Nombre'];?></h4>
+            <a href="<?php echo $re['Link'];?>" class="btn btn-success">Ver Más</a>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+  </section>
+  <!-- /SERVICIOS -->
 
+  <!-- ABAJO -->
+  <?php include("abajo.php"); ?>
+  <!-- /ABAJO -->
 
+  <!-- COPYRIGHT -->
+  <footer class="copy text-center py-3 bg-light">
+    2025 © Grupo Boson | <a href="https://www.boson.com.mx">boson.com.mx</a> | Todos los derechos reservados
+  </footer>
 
-<!---------------------- Nosotros -------------------->
+  <!-- NAUTILUS -->
+  <section class="fondo-negro text-center py-3">
+    <a href="http://nautilus360.com" target="_blank">
+      <img src="http://nautilus360.com/nautilus360.png" width="60" alt="Nautilus 360">
+    </a>
+  </section>
 
-<div class="seccion-color" id="seccion-color">
-<img src="imagenes/sombra-top.png" width="100%"/>
-<div class="container">
-<div class="row">
-
-
-<div class=" col-xs-12  col-sm-5 col-sm-offset-1  ">
-<?php include("conectarse.php");$consulta = mysqli_query($conexion,"select * from contenidos where seccion ='portada'"); while($re=mysqli_fetch_array($consulta)){ ?>
-      
-<h1> <div class="color1"><?php echo $re['titulo'];?></div></h1>
-<h3><?php echo $re['subtitulo'];?></h3>
-<div class="text-justify">
-<?php echo $re['contenido'];?>
-</div>
-<p>&nbsp;</p>
-<a href="<?php echo $re['Link'];?>"> <button class="btn btn-success"> Ver Más</button></a>
-<p>&nbsp;</p>
-<? } ?>
-</div>
-
-
-<div class="col-xs-12  col-sm-12  col-md-5 col-md-offset-1  text-center block" data-move-y="50px">
-<img src="imagenes/nosotros.png" width="100%">
-<p><br></p>
-</div>
-
-
-</div>  
-</div>   
-</div>  
-<!--- Nosotros --->
-
-
-
-
-<!--- Productos --->
-<div>
-<div class="container">
-<div class="row">
-
-<div class="col-xs-12 col-sm-12 text-center"> 
-<p><br></p>
-<h1 class="color2">Nuestros Servicios</h1>
-<p><br></p>
-</div> 
-
-<?php include("conectarse.php")
-;$consulta = mysqli_query($conexion,"select * from categorias where Estado ='Activo' and Menu ='Principal'  order by Id ASC"); while($re=mysqli_fetch_array($consulta)){ ?>
-
-<div class="col-xs-6  col-sm-4">
-
-<table width="90%" align="center" cellpadding="5" cellspacing="5">
-<tr>
-<td align="center">
-<a href="<?php echo $re['Link'];?>">
-<img src="imagenes/<?php echo $re['Foto'];?>" class="zoom" width="100%" ></a>
-
-<h4><?php echo $re['Nombre'];?></h4>
-<a href="<?php echo $re['Link'];?>">
-  <button class="btn btn-success">Ver Mas
-  </button></a>
-<p>&nbsp;</p>
-</td>
-</tr>
-</table>
-</div>
-
-<? } ?> 
-
-<div class="col-xs-12 col-sm-12 text-center"> 
-<p><br></p>
-<h1></h1>
-<p><br></p>
-</div> 
-
-
-</div> 
-</div> 
-</div> 
-<!--- Productos --->
-
-
-
-
-<!-------------------- ABAJO ---------------------->
-<?php include ("abajo.php"); ?>
-<!-------------------- ABAJO ---------------------->
-
-<!---------------- FINAl ---------------->
-  
-<section class="copy">
-	<div class="container">
-    	<div class=" col-xs-12  col-sm-12  col-md-12  text-center "> 
-   			2024 © Grupo Boson |  www.boson.com.mx | Todos los derechos reservados
-    	</div>
-	</div> 
-</section>    
-
-<!--------------- FINAl --------------->
-
-
-          
-<!---------------- NAUTILUS ---------------->
-  
-<section class="fondo-negro" id="abajo">
-	<div class="container">
-    	<div class=" col-xs-12  col-sm-12  col-md-12  text-center "> 
-            <p><a href="http://nautilus360.com" target="_blank"><img src="http://nautilus360.com/nautilus360.png" width="60" /></a></p>
-    	</div>
-	</div> 
-</section>    
-
-<!--------------- NAUTILUS --------------->
-
-<!-- UP DOWN -->
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script><script type="text/javascript" src="jquery.easing.1.3.js"></script><script type="text/javascript">
-            $(function() {
-                $('nav a').bind('click',function(event){
-                    var $anchor = $(this);
-                    
-                    $('html, body').stop().animate({
-                        scrollTop: $($anchor.attr('href')).offset().top
-                    }, 1500,'easeInOutExpo');
-
-                    event.preventDefault();
-                });
-            });
-        </script>
-<!-- UP DOWN -->
-
-<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.smoove.js"></script>
-	<script>$('.block').smoove({offset:'40%'});</script>
-
-    
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="jquery.smartmenus.js"></script>
-    <script type="text/javascript" src="jquery.smartmenus.bootstrap-4.js"></script>    
-    
-    
+  <!-- Scripts -->
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="js/jquery.smoove.js"></script>
+  <script>
+    $('.block').smoove({ offset: '40%' });
+  </script>
 
 </body>
 </html>
